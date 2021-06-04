cb8f3fc8399ab1444fc421a0c9018a76a67a25c7
Luis Fernando Spindola <58234526+luisfernandospindola@users.noreply.github.com>
2021-06-04 14:12:48 -0300

    Primeiro commit
    Primeiro commit.

---
+++ src/main/java/br/com/spindola/unicesumar/UnicesumarApplication.java
@@ -0,0 +1,13 @@
+package br.com.spindola.unicesumar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicesumarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicesumarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/spindola/unicesumar/UnicesumarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spindola.unicesumar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnicesumarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
