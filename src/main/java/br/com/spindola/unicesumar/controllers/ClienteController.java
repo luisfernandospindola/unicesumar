2c794f4bfee33366b7aa44a840959027e51822e0
Luis Fernando Spindola <spinluisfer@gmail.com>
2021-06-07 18:43:04 -0300

    adicionando novos controllers, e padrão MVC

--- src/main/java/br/com/spindola/unicesumar/controllers/ClienteController.java
+++ src/main/java/br/com/spindola/unicesumar/controllers/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.spindola.unicesumar.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.spindola.unicesumar.models.Cliente;
@@ -19,6 +20,11 @@ public class ClienteController {
 	@GetMapping("/{id}")
 	public Cliente obterClientePorId1(@PathVariable int id) {
 		return new Cliente(id, "Maria", "987.654.321-00");
+	}
+	
+	@GetMapping
+	public Cliente obterClientePorId2(@RequestParam(name = "id", defaultValue = "1") int id) {
+		return new Cliente(id, "João Augusto", "111.222.333-44");
 	}
 
 }
--- src/main/java/br/com/spindola/unicesumar/controllers/PrimeiroController.java
+++ src/main/java/br/com/spindola/unicesumar/controllers/PrimeiroController.java
@@ -1,7 +1,6 @@
 package br.com.spindola.unicesumar.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
